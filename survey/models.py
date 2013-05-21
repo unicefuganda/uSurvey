907ff6ebaafd131ca92e99a51d0aa2a5a79459c1
Sri Prasanna <k.sriprasanna@gmail.com>
2013-05-21 16:37:29 +0300

    fixing harvest

--- survey/models.py
+++ survey/models.py
@@ -4,3 +4,6 @@ from django_extensions.db.models import TimeStampedModel
 class Investigator(TimeStampedModel):
     name = models.CharField(max_length=100, blank=False, null=False)
     mobile_number = models.CharField(max_length=20, unique=True, null=False, blank=False)
+
+    class Meta:
+        app_label = 'survey'
