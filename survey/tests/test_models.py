58efe6a594bec07e10627c875307f16608c6c021
Sri Prasanna <k.sriprasanna@gmail.com>
2013-05-22 10:50:52 +0300

    + @mnandri - adding test for locations

--- survey/tests/test_models.py
+++ survey/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from survey.models import *
 from django.db import IntegrityError
+from rapidsms.contrib.locations.models import Location, LocationType
 
 
 class InvestigatorTest(TestCase):
@@ -14,3 +15,17 @@ class InvestigatorTest(TestCase):
     def test_validations(self):
         Investigator.objects.create(name="", mobile_number = "mobile_number")
         self.failUnlessRaises(IntegrityError, Investigator.objects.create, mobile_number = "mobile_number")
+
+class LocationTest(TestCase):
+
+    def test_store(self):
+        country = LocationType.objects.create(name='Country',slug='country')
+        district = LocationType.objects.create(name='District',slug='district')
+        uganda = Location.objects.create(name="Uganda", type=country)
+        kampala = Location.objects.create(name="Kampala", type=district, tree_parent=uganda)
+
+        u = Location.objects.get(type__name='Country',name='Uganda')
+        report_locations = u.get_descendants(include_self=True).all()
+        self.assertEqual(len(report_locations), 2)
+        self.assertIn(uganda, report_locations)
+        self.assertIn(kampala, report_locations)
