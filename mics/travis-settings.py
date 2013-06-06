26f2926792f545ff59ffab9336d6bbdddc9c9971
Sri Prasanna <k.sriprasanna@gmail.com>
2013-06-06 18:55:38 +0300

    adding travis specific settings

---
+++ mics/travis-settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "mics",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+    }
+}
