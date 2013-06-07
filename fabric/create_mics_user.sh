d35e4b7ac2378df63134a8941864952856b76cb3
Sri Prasanna <k.sriprasanna@gmail.com>
2013-06-07 11:12:44 +0300

    removing fabric scripts

--- fabric/create_mics_user.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
-useradd -m mics
-adduser mics sudo
-usermod -s /bin/bash mics
-echo 'mics ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-echo "Please add your ssh keys to authorized_keys file"
-echo "Also add your github rsa key"
--- fabric/setup_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /var/www/apps/env/mics/bin/activate
-fab -f setup_environment.py ulous
-fab -f deploy.py production deploy
