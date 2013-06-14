eefef81b0b09d4e5a7c2493c47dc4d55373ac73f
Sri Prasanna <k.sriprasanna@gmail.com>
2013-06-14 18:19:42 +0300

    adding shiny new ussd simulator

---
+++ survey/static/js/simulator.js
@@ -0,0 +1,59 @@
+;
+
+jQuery(function(){
+  var transactionId = $("#transactionId"),
+      transactionTime = $("#transactionTime"),
+      response_true = $("#response-true"),
+      response_false = $("#response-false"),
+      server_response = $("#server-response"),
+      user_response = $("#ussdRequestString"),
+      simulator = $("#simulator-form"),
+      timer = $("#timer"),
+      delay = 5;
+
+  function form_reset () {
+    transactionId.val(Math.floor((Math.random()*100000)));
+    response_true.removeAttr('checked');
+    response_false.prop('checked', true);
+    server_response.text('');
+    user_response.val('');
+  }
+
+  function startTimer () {
+    timer.countdown({
+      render: function(data) {
+        $(this.el).text(this.leadingZeros(data.min, 2) + " min " + this.leadingZeros(data.sec, 2) + " sec");
+      },
+      onEnd: function(){
+        $(this.el).addClass('ended');
+        form_reset();
+      }
+    });
+    timer.removeClass('ended').data('countdown').update(+(new Date) + 180000).start();
+  }
+
+  form_reset();
+
+  function ussd_submit () {
+    $.post('/ussd', simulator.serializeArray(), function(data){
+      if (response_false.is(':checked'))
+        startTimer();
+      var response = data.split("&action=")[0];
+      response = response.split("responseString=")[1];
+      server_response.text(response);
+    }).fail(function(data){
+      alert("There is an error!");
+    }).always(function(){
+      response_false.removeAttr('checked');
+      response_true.prop('checked', true);
+      user_response.val('');
+      $('#form-control, #loader').toggleClass('hide');
+    });
+  }
+
+  simulator.submit(function(){
+    $('#form-control, #loader').toggleClass('hide');
+    window.setTimeout(ussd_submit, delay * 1000);
+    return false;
+  });
+});
