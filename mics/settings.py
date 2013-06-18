540f5655086b15cc5aec85e5888fbeaa04ba916c
Sri Prasanna <k.sriprasanna@gmail.com>
2013-06-18 15:52:20 +0300

    adding memcache

--- mics/settings.py
+++ mics/settings.py
@@ -21,6 +21,18 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'LOCATION': 'memcached:11211',
+        'TIMEOUT': 500,
+        'BINARY': False,
+        'OPTIONS': {  # Maps to pylibmc "behaviors"
+            'tcp_nodelay': True,
+            'ketama': True
+        }
+    }
+}
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
