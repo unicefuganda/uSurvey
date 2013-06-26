9b8f78e779b72fd68e661c99838e2e19ab28d65c
NavaL <mnandri@thoughtworks.com>
2013-06-26 16:06:50 +0300

    hHead form test

--- survey/tests/forms/test_householdHead_form.py
+++ survey/tests/forms/test_householdHead_form.py
@@ -1,42 +1,77 @@
 from django.test import TestCase
-from survey.forms.household import *
+from survey.forms.householdHead import *
 from survey.models import Investigator
 
 class HouseholdHeadFormTest(TestCase):
 
+    def setUp(self):
+        self.form_data = {
+                        'surname': 'household',
+                        'first_name': 'bla',
+                        'male': 't',
+                        'age':'23',
+                        'level_of_education':'Primary',
+                        'occupation':'Brewing',
+                        'resident_since':'3',
+                        'time_measure':'Years',
+                    }
+
     def test_valid(self):
-        form_data = {
-                        'number_of_males': '6',
-                        'number_of_females': '6',
-                        'size': '12',
-                    }
-        household_form = HouseholdForm(form_data)
-        self.assertTrue(household_form.is_valid())
-        investigator = Investigator.objects.create(name="test")
-        household_form.instance.investigator = investigator
-        household = household_form.save()
-        self.failUnless(household.id)
-        household_retrieved = Household.objects.get(investigator=investigator)
-        self.assertEqual(household_retrieved, household)
+        hHead_form = HouseholdHeadForm(self.form_data)
+        self.assertTrue(hHead_form.is_valid())
+        household = Household.objects.create()
+        hHead_form.instance.household = household
+        hHead = hHead_form.save()
+        self.failUnless(hHead.id)
+        hHead_retrieved = HouseholdHead.objects.get(household=household)
+        self.assertEqual(hHead_retrieved, hHead)
 
-    def test_total_wrong(self):
-        form_data = {
-                        'number_of_males': 6,
-                        'number_of_females': 6,
-                        'size': 0,
-                    }
-        household_form = HouseholdForm(form_data)
-        self.assertFalse(household_form.is_valid())
-        message = "Total number of household members does not match female and male ones."
-        self.assertEquals(household_form.errors['size'], [message])
+    def test_age(self):
+        data = self.form_data
+        data['age']=9
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = "Ensure this value is greater than or equal to 10."
+        self.assertEquals(hHead_form.errors['age'], [message])
 
-    def test_clean_override_with_string_not_integer(self):
-        form_data = {
-                        'number_of_males': '6',
-                        'number_of_females': '6',
-                        'size': '0',
-                    }
-        household_form = HouseholdForm(form_data)
-        self.assertFalse(household_form.is_valid())
-        message = "Total number of household members does not match female and male ones."
-        self.assertEquals(household_form.errors['size'], [message])
+        data['age']=100
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = "Ensure this value is less than or equal to 99."
+        self.assertEquals(hHead_form.errors['age'], [message])
+
+
+    def test_required_fields(self):
+        data = self.form_data
+        del data['age']
+        del data['surname']
+        del data['first_name']
+
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = "This field is required."
+        self.assertEquals(hHead_form.errors['age'], [message])
+        self.assertEquals(hHead_form.errors['surname'], [message])
+        self.assertEquals(hHead_form.errors['first_name'], [message])
+
+    def test_positive_resident_since(self):
+        data = self.form_data
+
+        data['resident_since'] = -6
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = "Ensure this value is greater than or equal to 0."
+        self.assertEquals(hHead_form.errors['resident_since'], [message])
+
+        data['resident_since'] = 'not a number'
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = 'Enter a whole number.'
+        self.assertEquals(hHead_form.errors['resident_since'], [message])
+
+        data['resident_since'] = None
+        hHead_form = HouseholdHeadForm(data)
+        self.assertFalse(hHead_form.is_valid())
+        message = 'This field is required.'
+        self.assertEquals(hHead_form.errors['resident_since'], [message])
+
