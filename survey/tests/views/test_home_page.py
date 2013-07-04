a215882d4a30e65a3bfe345b11fbedd82fda1a36
NavaL <mnandri@thoughtworks.com>
2013-07-04 18:36:16 +0300

    added dummy home page and rearranged urls.py

---
+++ survey/tests/views/test_home_page.py
@@ -0,0 +1,17 @@
+import json
+
+from django.test import TestCase
+from django.test.client import Client
+from mock import *
+from survey.views.home_page import *
+
+
+class HomepageViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+        templates = [template.name for template in response.templates]
+        self.assertIn('home/index.html', templates)
--- survey/urls.py
+++ survey/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from survey.views.investigator import *
-from survey.views.ussd import *
-from survey.views.household import *
 
 urlpatterns = patterns('',
-    url(r'^investigators/$', list_investigators, name="investigators_page"),
-    url(r'^investigators/filter/(?P<location_id>\d+)/$', filter_list_investigators),
-    url(r'^investigators/filter/$', list_investigators),
-    url(r'^investigators/new/$', new_investigator, name="new_investigator_page"),
-    url(r'^investigators/locations', get_locations, name="locations_autocomplete"),
-    url(r'^investigators/check_mobile_number', check_mobile_number, name="check_mobile_number"),
+    url(r'^$', 'survey.views.home_page.home', name='home_page'),
+    url(r'^investigators/$', 'survey.views.investigator.list_investigators', name="investigators_page"),
+    url(r'^investigators/filter/(?P<location_id>\d+)/$', 'survey.views.investigator.filter_list_investigators'),
+    url(r'^investigators/filter/$', 'survey.views.investigator.list_investigators'),
+    url(r'^investigators/new/$', 'survey.views.investigator.new_investigator', name="new_investigator_page"),
+    url(r'^investigators/locations', 'survey.views.investigator.get_locations', name="locations_autocomplete"),
+    url(r'^investigators/check_mobile_number', 'survey.views.investigator.check_mobile_number', name="check_mobile_number"),
     url(r'^ussd/simulator', TemplateView.as_view(template_name="ussd/simulator.html")),
-    url(r'^ussd', ussd, name="ussd"),
-    url(r'^households/new/$', new, name="new_household_page"),
-    url(r'^households/investigators', get_investigators, name='load_investigators'),
+    url(r'^ussd', 'survey.views.ussd.ussd', name="ussd"),
+    url(r'^households/new/$', 'survey.views.household.new', name="new_household_page"),
+    url(r'^households/investigators', 'survey.views.household.get_investigators', name='load_investigators'),
 )
