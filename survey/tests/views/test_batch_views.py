65d1dc70024e3e69d4414e10c84552c649f642d5
Andrew Mukiza <mandy.rgf@gmail.com>
2013-09-02 15:57:44 +0300

    Refactored test batch views to use base test

--- survey/tests/views/test_batch_views.py
+++ survey/tests/views/test_batch_views.py
@@ -4,26 +4,21 @@ from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from rapidsms.contrib.locations.models import Location, LocationType
 from django.contrib import messages
-
+from survey.tests.base_test import BaseTest
 from survey.models import *
 from survey.forms.batch import BatchForm
 
 
-class BatchViews(TestCase):
+class BatchViews(BaseTest):
 
     def setUp(self):
         self.client = Client()
-        raj = User.objects.create_user('Rajni', 'rajni@kant.com', 'I_Rock')
         user_without_permission = User.objects.create_user(username='useless', email='rajni@kant.com', password='I_Suck')
-
-        some_group = Group.objects.create(name='some group')
-        auth_content = ContentType.objects.get_for_model(Permission)
-        permission, out = Permission.objects.get_or_create(codename='can_view_batches', content_type=auth_content)
-        some_group.permissions.add(permission)
-        some_group.user_set.add(raj)
+        raj = self.assign_permission_to(User.objects.create_user('Rajni', 'rajni@kant.com', 'I_Rock'), 'can_view_batches')
+        raj = self.assign_permission_to(raj, 'can_view_investigators')
 
         self.client.login(username='Rajni', password='I_Rock')
-
+        
         self.batch = Batch.objects.create(order = 1, name = "Batch A")
         district = LocationType.objects.create(name=PRIME_LOCATION_TYPE, slug=PRIME_LOCATION_TYPE)
         self.kampala = Location.objects.create(name="Kampala", type=district)
@@ -67,13 +62,6 @@ class BatchViews(TestCase):
         for loc in [self.kampala, self.kampala_city, self.bukoto, self.kamoja]:
             self.assertFalse(self.batch.is_open_for(loc))
 
-    def assert_restricted_permission_for(self, url):
-        self.client.logout()
-
-        self.client.login(username='useless', password='I_Suck')
-        response = self.client.get(url)
-
-        self.assertRedirects(response, expected_url='/accounts/login/?next=%s'%url, status_code=302, target_status_code=200, msg_prefix='')
 
     def test_restricted_permssion(self):
         self.assert_restricted_permission_for('/batches/')
