0e356369e348e4a6b3ecbb02e2c030ddc6ac3716
Andrew Mukiza <mandy.rgf@gmail.com>
2013-09-04 11:23:03 +0300

    [Andrew/Pooja]: adding multiple select on groups html

--- survey/static/js/application.js
+++ survey/static/js/application.js
@@ -1,7 +1,7 @@
 ;
 $(function(){
   $(".chzn-select").chosen();
-  $("#bulk-sms-locations").multiSelect();
+  $(".multi-select").multiSelect();
   $(".no-paste").on("paste", function(e){
     e.preventDefault();
   });
