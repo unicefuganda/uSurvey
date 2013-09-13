00b6fb16e2f9be1cfe48f6b3563511baadf669a9
Andrew Mukiza <mandy.rgf@gmail.com>
2013-09-13 17:58:11 +0300

    mend

--- survey/features/index-steps.py
+++ survey/features/index-steps.py
@@ -26,7 +26,7 @@ def clear_screenshots():
 
 
 def open_browser():
-    world.browser = Browser("chrome")
+    world.browser = Browser("phantomjs")
 
 @after.each_scenario
 def take_screenshot(scenario):
