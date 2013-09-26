c9081517610d6aeb5c25a84de26a33cbeafc670d
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-09-26 10:59:09 +0300

    Brain/Pooja: Moving add subquestion link back to index page independent of batch

--- survey/views/home_page.py
+++ survey/views/home_page.py
@@ -1,10 +1,5 @@
-import json
-
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib import messages
 
-from survey.investigator_configs import *
 
 def home(request):
     return render(request, 'home/index.html')
