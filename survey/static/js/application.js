ceecc44311fd87f7eb32cc9367e9d589ebccc8ea
Andrew Mukiza <mandy.rgf@gmail.com>
2013-09-30 12:42:50 +0300

    fixing cancel buttons

--- survey/static/js/application.js
+++ survey/static/js/application.js
@@ -1,14 +1,18 @@
 ;
 $(function(){
-  $(".chzn-select").chosen();
-  $(".multi-select").multiSelect();
-  $(".no-paste").on("paste", function(e){
-    e.preventDefault();
-  });
-});
-
-$(function(){
+    $(".chzn-select").chosen();
+    $(".multi-select").multiSelect();
+    $(".no-paste").on("paste", function(e){
+      e.preventDefault();
+    });
+    
     $('.datepicker').datepicker({dateFormat:'yy-mm-dd'});
     $('.modal-body .form-actions a').removeAttr('href').attr('data-dismiss', 'modal');
-});
 
+    $('a[name=cancel_button]').on('click', function(){
+        goBack();
+    });
+    function goBack() {
+        window.location = document.referrer;
+     }
+});
