3c75870d147ba2a012f452a40eba23258162cd6d
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-10-15 17:20:59 +0530

    adding case insensitive query for country choices

--- survey/forms/location_hierarchy.py
+++ survey/forms/location_hierarchy.py
@@ -3,6 +3,9 @@ from rapidsms.contrib.locations.models import Location
 
 
 class LocationHierarchyForm(forms.Form):
-    COUNTRY_CHOICES = [(country.id,country.name) for country in Location.objects.filter(type__name='country')]
-    country = forms.ChoiceField(label='Country', choices=COUNTRY_CHOICES, widget=forms.Select, required=True)
-    levels = forms.CharField(label= 'Level1', max_length=50, required=False)
+    def __init__(self):
+        super(LocationHierarchyForm, self).__init__()
+        self.COUNTRY_CHOICES = [(country.id,country.name) for country in Location.objects.filter(type__name__iexact='country')]
+
+        self.fields['country'] = forms.ChoiceField(label='Country', choices=self.COUNTRY_CHOICES, widget=forms.Select, required=True)
+        self.fields['levels'] = forms.CharField(label= 'Level1', max_length=50, required=False)
--- survey/tests/forms/test_location_hierarchy_form.py
+++ survey/tests/forms/test_location_hierarchy_form.py
@@ -1,9 +1,14 @@
+from django.template.defaultfilters import slugify
 from django.test.testcases import TestCase
-from rapidsms.contrib.locations.models import Location
+from rapidsms.contrib.locations.models import Location, LocationType
 from survey.forms.location_hierarchy import LocationHierarchyForm
 
 
 class LocationHierarchyFormTest(TestCase):
+    def setUp(self):
+        country = LocationType.objects.create(name='country',slug=slugify('country'))
+        self.uganda = Location.objects.create(type=country, name='Uganda')
+
     def test_knows_the_fields_in_form(self):
         hierarchy_form = LocationHierarchyForm()
 
@@ -19,3 +24,16 @@ class LocationHierarchyFormTest(TestCase):
 
         [self.assertIn((country_option.id, country_option.name), country_choices) for country_option in all_countries]
 
+    def test_should_populate_countries_name_case_insensitive(self):
+        LocationType.objects.all().delete()
+        Location.objects.all().delete()
+        country_1 = LocationType.objects.create(name='Country',slug=slugify('Country'))
+        some_country = Location.objects.create(type=country_1, name='some_country')
+
+        field = 'country'
+        hierarchy_form = LocationHierarchyForm()
+        all_countries = Location.objects.filter(type__name='country')
+        country_choices = hierarchy_form.fields[field].choices
+
+        [self.assertIn((country_option.id, country_option.name), country_choices) for country_option in all_countries]
+        self.assertIn((some_country.id, some_country.name), country_choices)
