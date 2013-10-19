9c55225b4139114660f53992ed27a425d4a10376
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-10-19 11:16:02 +0530

    adding functional test for location hierarchy

--- survey/features/location_hierarchy.feature
+++ survey/features/location_hierarchy.feature
@@ -1,3 +1,16 @@
 Feature: Location hierarchy
 
   Scenario: Add location hierarchy
+    Given I am logged in as researcher
+    And I have a country
+    And I visit add location hierarchy page
+    Then I should see text message
+    And I should see country dropdown
+    And I should see country present in dropdown
+    And I should see a row for level details field
+    And I should see a link to add another row
+    And I should see a link to remove a row
+    When I click add row link
+    Then I should see anther row for levels details field
+    When I click remove row link
+    Then I should see row for levels details field removed
--- survey/features/location_hierarchy_steps.py
+++ survey/features/location_hierarchy_steps.py
@@ -1,52 +1,62 @@
-#from lettuce import step, world
-#from survey.features.page_objects.location_hierarchy import AddLocationHierarchyPage
-#
-#
-#@step(u'And I visit add location hierarchy page')
-#def and_i_visit_add_location_hierarchy_page(step):
-#    world.page = AddLocationHierarchyPage(world.browser)
-#    world.page.visit()
-#
-#@step(u'Then I should see text message')
-#def then_i_should_see_text_message(step):
-#    world.page.is_text_present('Create geographical location hierarchy')
-#
-#@step(u'And I should see country dropdown')
-#def and_i_should_see_country_dropdown(step):
-#    assert world.browser.find_by_css('#id_country')
-#
-#@step(u'And I should see level1 text field')
-#def and_i_should_see_level1_text_field(step):
-#    assert world.browser.find_by_css('#id_levels')
-#
-#@step(u'And I should see a plus icon')
-#def and_i_should_see_a_plus_icon(step):
-#    assert world.browser.find_by_css('.add-option')
-#
-#@step(u'When I click add-level icon')
-#def when_i_click_add_level_icon(step):
-#    world.browser.find_by_css('.add-option').first.click()
-#
-#@step(u'Then I should see another level field')
-#def then_i_should_see_another_level_field(step):
-#    assert len(world.browser.find_by_name('levels')) == 2
-#
-#@step(u'And I should see another plus icon')
-#def and_i_should_see_another_plus_icon(step):
-#    assert len(world.browser.find_by_css('.add-option')) == 2
-#
-#@step(u'And I should see remove icon')
-#def and_i_should_see_remove_icon(step):
-#    assert world.browser.find_by_css('.remove-option')
-#
-#@step(u'When I click remove level icon')
-#def when_i_click_remove_level_icon(step):
-#    world.browser.find_by_css('.remove-option').first.click()
-#
-#@step(u'Then I should see only one level field')
-#def then_i_should_see_only_one_level_field(step):
-#    assert len(world.browser.find_by_name('levels')) == 1
-#
-#@step(u'And I should not see remove icon')
-#def and_i_should_not_see_remove_icon(step):
-#    assert not world.browser.find_by_css('.remove-option')
+from lettuce import step, world
+from rapidsms.contrib.locations.models import Location, LocationType
+from survey.features.page_objects.location_hierarchy import AddLocationHierarchyPage
+
+
+@step(u'And I have a country')
+def and_i_have_a_country(step):
+    Location.objects.create(name='Some Country',type=LocationType.objects.create(name='country',slug='country'))
+
+@step(u'And I visit add location hierarchy page')
+def and_i_visit_add_location_hierarchy_page(step):
+    world.page = AddLocationHierarchyPage(world.browser)
+    world.page.visit()
+
+@step(u'Then I should see text message')
+def then_i_should_see_text_message(step):
+    world.page.is_text_present('Create geographical location hierarchy')
+
+@step(u'And I should see country dropdown')
+def and_i_should_see_country_dropdown(step):
+    assert world.browser.find_by_css('#id_country')
+
+@step(u'And I should see country present in dropdown')
+def and_i_should_see_country_present_in_dropdown(step):
+    world.page.see_select_option(['Some Country'],'country')
+
+@step(u'And I should see a row for level details field')
+def and_i_should_see_a_row_for_level_details_field(step):
+    world.browser.find_by_name('form-0-levels')
+    world.browser.find_by_name('form-0-required')
+    world.browser.find_by_name('form-0-has_code')
+    world.browser.find_by_name('form-0-code')
+
+@step(u'And I should see a link to add another row')
+def and_i_should_see_a_link_to_add_another_row(step):
+    world.page.find_link_by_text('add another')
+
+@step(u'And I should see a link to remove a row')
+def and_i_should_see_a_link_to_remove_a_row(step):
+    world.page.find_link_by_text('remove')
+
+@step(u'When I click add row link')
+def when_i_click_add_row_link(step):
+    world.page.click_link_by_text('add another')
+
+@step(u'Then I should see anther row for levels details field')
+def then_i_should_see_anther_row_for_levels_details_field(step):
+    world.browser.find_by_name('form-1-levels')
+    world.browser.find_by_name('form-1-required')
+    world.browser.find_by_name('form-1-has_code')
+    world.browser.find_by_name('form-1-code')
+
+@step(u'When I click remove row link')
+def when_i_click_remove_row_link(step):
+    world.browser.find_by_css('.delete-row').last.click()
+
+@step(u'Then I should see row for levels details field removed')
+def then_i_should_see_row_for_levels_details_field_removed(step):
+    world.page.field_not_present('form-0-levels')
+    world.page.field_not_present('form-0-required')
+    world.page.field_not_present('form-0-has_code')
+    world.page.field_not_present('form-0-code')
