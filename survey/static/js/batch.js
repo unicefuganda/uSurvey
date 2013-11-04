56bf66eb1b391a2be9da36a752104f55a00b65bd
Andrew Mukiza <mandy.rgf@gmail.com>
2013-11-04 13:43:08 +0300

    fixing the bug on list batch questions where some questions were not showing

--- survey/static/js/batch.js
+++ survey/static/js/batch.js
@@ -1,28 +1,10 @@
 ;
 
-function set_display(text, set){
-    var all_elements = $('.ms-selectable').children().children();
-
-    for(var counter=0; counter<all_elements.length; counter++)
-    {
-        element = all_elements[counter];
-
-        if(set){
-         if (text == element.childNodes[0].childNodes[0].nodeValue){
-             element.style.display = ''
-         }
-        }
-        else{
-            element.style.display = 'none'
-        }
-    }
-};
-
-
 jQuery(function($){
   if ($('#assign_question_group').val()){
-        load_questions_for_batch_and_group();
+        load_questions_for_filter();
     }
+
   $('.switch').on('switch-change', function(e, data){
     var current_switch = $(this);
     current_switch.parent().find('.error').remove();
@@ -66,25 +48,26 @@ jQuery(function($){
 
 
   $('#assign_question_group').on('change', function(){
-      load_questions_for_batch_and_group();
+      load_questions_for_filter();
   });
   $('#assign_module').on('change', function(){
-      load_questions_for_batch_and_group();
+      load_questions_for_filter();
   });
 
 });
 
 
-function load_questions_for_batch_and_group(){
+
+function load_questions_for_filter(){
     var group_selected = $('#assign_question_group').val();
     var module_selected = $('#assign_module').val(),
         batch_id = $("#batch_id").val(),
         url = '/batches/'+ batch_id +'/questions/groups/'+ group_selected + '/module/' + module_selected +'/';
     $.getJSON(url, function(data){
-        set_display('', false);
+         $('.ms-selectable').children().children().hide();
         $.each(data, function(){
-            representation = this.text + ": (" + this.answer_type.toUpperCase() + ")"
-            set_display(representation , true);
+            var representation = this.text + ": (" + this.answer_type.toUpperCase() + ")"
+             $('.ms-selectable li:contains(' + representation + ')').show();
         });
     });
 }
