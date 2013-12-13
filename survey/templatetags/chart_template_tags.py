5ffc474dce39ff1027a0f1946553b7a129d95f1e
Andrew Mukiza <mandy.rgf@gmail.com>
2013-12-13 19:28:55 +0300

    removed unused methods in chart_templage_tags

--- survey/templatetags/chart_template_tags.py
+++ survey/templatetags/chart_template_tags.py
@@ -3,57 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter
-def title_for_barchart(formula, locations):
-    title = "%s for %s" % (formula.indicator.name, title_for_location_type(locations))
-    return title
-
-@register.filter
-def location_names(hierarchial_data):
-    names = [str(location.name) for location in hierarchial_data.keys()]
-    return names
-
-@register.filter
-def title_for_location_type(locations):
-    next_type_in_hierarchy = locations.next_type_in_hierarchy()
-    if next_type_in_hierarchy:
-        return next_type_in_hierarchy.name
-
-@register.filter
-def get_values(hierarchial_data):
-    return hierarchial_data.values()
-
-@register.filter
-def is_float(value):
-    return type(value) == float
-
-@register.filter
-def can_display_location_based_chart(locations):
-    return title_for_location_type(locations) != None
-
-@register.filter
-def household_names(households):
-    names = [str(household.get_head().surname) for household in households.keys()]
-    return names
-
-@register.filter
-def get_numerator_denominator_values(households_data, formula):
-    aggregated_data = {formula.numerator.text: [], formula.denominator.text: []}
-    for household, answers in households_data.items():
-        for question, answer in answers.items():
-            aggregated_data[question.text].append(answer)
-    result = []
-    for name, data in aggregated_data.items():
-        result.append({
-            'name': str(name),
-            'data': data
-        })
-    return result
-
-@register.filter
-def multi_choice_options(computed_value):
-    return [str(key) for key in computed_value.keys()]
-
-@register.filter
 def get_computational_value_by_answer(hierarchial_data):
     options = {}
     for location, answers in hierarchial_data.items():
@@ -68,11 +17,3 @@ def get_computational_value_by_answer(hierarchial_data):
             'data': value
         })
     return data
-
-@register.filter
-def get_numerator(answer, formula):
-    return answer[formula.numerator].text
-
-@register.filter
-def get_denominator(answer, formula):
-    return answer[formula.denominator]
