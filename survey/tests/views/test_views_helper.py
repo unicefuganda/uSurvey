1d327e9522e160991ab4c2dd0a84eda1533f71ed
vivitc <vivitc@gmail.com>
2014-07-10 18:15:12 +0300

    [Naval, Vivi] Adding get_descendants for location in ViewsHelper.

--- survey/tests/views/test_views_helper.py
+++ survey/tests/views/test_views_helper.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
 from rapidsms.contrib.locations.models import Location, LocationType
-from survey.views.views_helper import contains_key
+from survey.models import LocationTypeDetails
+from survey.views.views_helper import contains_key, get_descendants
 
 class ViewsHelperTest(TestCase):
     def test_contains_key(self):
@@ -10,3 +11,38 @@ class ViewsHelperTest(TestCase):
         self.assertFalse(contains_key({'bla':'-1'}, 'bla'))
         self.assertFalse(contains_key({'bla':''}, 'bla'))
         self.assertFalse(contains_key({'bla':'NOT_A_DIGIT'}, 'bla'))
+
+    def test_get_descendants(self):
+        country = LocationType.objects.create(name='Country', slug='country')
+        region = LocationType.objects.create(name='Region', slug='region')
+        city = LocationType.objects.create(name='City', slug='city')
+        parish = LocationType.objects.create(name='Parish', slug='parish')
+        village = LocationType.objects.create(name='Village', slug='village')
+        subcounty = LocationType.objects.create(name='Subcounty', slug='subcounty')
+
+        africa = Location.objects.create(name='Africa', type=country)
+        LocationTypeDetails.objects.create(country=africa, location_type=country)
+        LocationTypeDetails.objects.create(country=africa, location_type=region)
+        LocationTypeDetails.objects.create(country=africa, location_type=city)
+        LocationTypeDetails.objects.create(country=africa, location_type=parish)
+        LocationTypeDetails.objects.create(country=africa, location_type=village)
+        LocationTypeDetails.objects.create(country=africa, location_type=subcounty)
+
+        uganda = Location.objects.create(name='Uganda', type=region, tree_parent=africa)
+
+        abim = Location.objects.create(name='ABIM', tree_parent=uganda, type=city)
+
+        abim_son = Location.objects.create(name='LABWOR', tree_parent=abim, type=parish)
+
+        abim_son_son = Location.objects.create(name='KALAKALA', tree_parent=abim_son, type=village)
+        abim_son_daughter = Location.objects.create(name='OYARO', tree_parent=abim_son, type=village)
+
+        abim_son_daughter_daughter = Location.objects.create(name='WIAWER', tree_parent=abim_son_daughter, type=subcounty)
+
+        abim_son_son_daughter = Location.objects.create(name='ATUNGA', tree_parent=abim_son_son, type=subcounty)
+        abim_son_son_son = Location.objects.create(name='WICERE', tree_parent=abim_son_son, type=subcounty)
+
+        expected_location_descendants = [abim, abim_son, abim_son_son, abim_son_daughter, abim_son_son_daughter,
+                                         abim_son_son_son, abim_son_daughter_daughter]
+
+        self.assertItemsEqual(expected_location_descendants, get_descendants(abim))
--- survey/views/views_helper.py
+++ survey/views/views_helper.py
@@ -1,5 +1,3 @@
-from rapidsms.contrib.locations.models import *
-
 
 def contains_key(params, key):
     return params.has_key(key) and params[key].isdigit()
@@ -7,3 +5,12 @@ def contains_key(params, key):
 
 def is_not_digit_nor_empty(params, key):
     return key in params and not (params[key] == '' or params[key].isdigit())
+
+def get_descendants(location):
+    children = location.get_children()
+    if not children.exists():
+        return [location]
+    result = [location]
+    for child in children:
+        result.extend(get_descendants(child))
+    return result
