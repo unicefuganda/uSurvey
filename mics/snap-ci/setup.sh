d1fa3156fa135314332c725dd3a988cd920b9229
NavaL <mnandri@thoughtworks.com>
2014-08-25 19:01:54 +0300

    installing memcached in snap-ci and by-passing coveralls for now

--- mics/snap-ci/setup.sh
+++ mics/snap-ci/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum install memcached python-memcached  -y
+sudo /etc/init.d/memcached start
+
 cd ..
 virtualenv mics_env
 source mics_env/bin/activate
 cd -
 pip install -r pip-requires.txt
-pip install coveralls
 cp mics/snap-ci/snap-settings.py mics/localsettings.py
 ./manage.py syncdb --noinput
 ./manage.py migrate
