0dfc579d0503172d7713c756ad89f9adfa5c3805
NavaL <mnandri@thoughtworks.com>
2014-08-25 19:01:54 +0300

    installing memcached in snap-ci and by-passing coveralls for now

--- mics/snap-ci/setup.sh
+++ mics/snap-ci/setup.sh
@@ -2,6 +2,15 @@
 
 sudo yum -y groupinstall "Development Tools"
 sudo yum install memcached python-memcached  -y
+wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar -xvf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
+sudo ./configure
+sudo make
+sudo make install
+
+cd ..
+
 sudo /etc/init.d/memcached start
 
 cd ..
