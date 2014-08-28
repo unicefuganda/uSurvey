96340ebbdac524527e726b7d4c2d015d61491d6a
timothyakampa <timothyakampa@gmail.com>
2014-08-28 16:54:12 +0300

    Updates adding phantomjs 1.9.1

--- mics/snap-ci/setup.sh
+++ mics/snap-ci/setup.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-function install_phantomjs{
-    wget http://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-    tar xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
-    sudo rm -rf /opt/local/phantomjs
-    sudo mv  phantomjs-1.9.1-linux-x86_64 /opt/local/phantomjs
-}
-
-install_phantomjs
+wget http://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
+tar xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
+sudo rm -rf /opt/local/phantomjs
+sudo mv  phantomjs-1.9.1-linux-x86_64 /opt/local/phantomjs
 
 sudo yum remove -y libmemcached libmemcached-devel
 sudo wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
