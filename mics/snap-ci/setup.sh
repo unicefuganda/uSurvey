5a80627fbdd5d0c87b5390711a5d6ecd19dbc69a
timothyakampa <timothyakampa@gmail.com>
2014-08-28 14:42:12 +0300

    [Tim, NavaL] Removing coveralls from snap-config

--- mics/snap-ci/setup.sh
+++ mics/snap-ci/setup.sh
@@ -10,7 +10,6 @@ virtualenv mics_env
 source mics_env/bin/activate
 cd -
 pip install -r pip-requires.txt
-pip install coveralls
 cp mics/snap-ci/snap-settings.py mics/localsettings.py
 ./manage.py syncdb --noinput
 ./manage.py migrate
