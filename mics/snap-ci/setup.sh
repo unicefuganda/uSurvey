1c88d10188a6f771f22dc58ff1e495e3e53ec931
timothyakampa <timothyakampa@gmail.com>
2014-08-28 15:02:08 +0300

    added memcache to snap configs

--- mics/snap-ci/setup.sh
+++ mics/snap-ci/setup.sh
@@ -4,6 +4,8 @@ sudo wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.
 sudo wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 sudo yum --enablerepo=remi install -y libmemcached-last libmemcached-last-devel
+sudo yum install -y memcached
+sudo service memcached restart
 
 cd ..
 virtualenv mics_env
