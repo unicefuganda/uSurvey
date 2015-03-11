9da0958fa90054ef806f3847ac49f870cd9fdf89
antsmc2 <antsmc2@yahoo.com>
2015-03-11 21:23:37 +0100

    fixed another issue with skipped questions.

--- survey/odk/utils/odk_helper.py
+++ survey/odk/utils/odk_helper.py
@@ -187,12 +187,13 @@ def process_submission(investigator, xml_file, media_files=[], request=None):
     response_dict = _get_responses(survey_tree, survey)
     treated_batches = {}
     for (b_id, q_id), answer in response_dict.items():
-        question = Question.objects.get(pk=q_id)
         batch = treated_batches.get(b_id, Batch.objects.get(pk=b_id))
-        if question.answer_type in [Question.AUDIO, Question.IMAGE, Question.VIDEO]:
-            answer = media_files.get(answer, None)
-        created = register_member_answer(investigator, question, member, answer, batch)
-        member_completion_report[(b_id, q_id)] = created
+        if q_id: #only attempt to handle relevant questions
+            question = Question.objects.get(pk=q_id)
+            if question.answer_type in [Question.AUDIO, Question.IMAGE, Question.VIDEO]:
+                answer = media_files.get(answer, None)
+            created = register_member_answer(investigator, question, member, answer, batch)
+            member_completion_report[(b_id, q_id)] = created
         if b_id not in treated_batches.keys():
             treated_batches[b_id] = batch
     submission = ODKSubmission.objects.create(investigator=investigator, 
