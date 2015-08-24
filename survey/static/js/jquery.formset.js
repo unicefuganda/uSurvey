73ad2c97b5abc0e9bf8f43de5d035ce92456de28
antsmc2 <antsmc2@yahoo.com>
2015-08-24 21:58:06 +0100

    Updated Add Interview Form, Home Page, Styles and Images

--- survey/static/js/jquery.formset.js
+++ survey/static/js/jquery.formset.js
@@ -39,15 +39,15 @@
                 if (row.is('TR')) {
                     // If the forms are laid out in table rows, insert
                     // the remove button into the last table cell:
-                    row.children(':last').append('<a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + '</a>');
+                    row.children(':last').append('<a class="btn btn_primary pull-right" href="javascript:void(0)"><i class="icon-minus"></i> ' + options.deleteText + '</a>');
                 } else if (row.is('UL') || row.is('OL')) {
                     // If they're laid out as an ordered/unordered list,
                     // insert an <li> after the last list item:
-                    row.append('<li><a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a></li>');
+                    row.append('<a class="btn btn_primary pull-right" href="javascript:void(0)"><i class="icon-minus"></i> ' + options.deleteText +'</a>');
                 } else {
                     // Otherwise, just insert the remove button as the
                     // last child element of the form's container:
-                    row.append('<a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a>');
+                    row.append('<a class="btn btn_primary pull-right" href="javascript:void(0)"><i class="icon-minus"></i> ' + options.deleteText +'</a>');
                 }
                 row.find('a.' + options.deleteCssClass).click(function() {
                     var row = $(this).parents('.' + options.formCssClass),
@@ -127,12 +127,12 @@
                 // If forms are laid out as table rows, insert the
                 // "add" button in a new table row:
                 var numCols = $$.eq(0).children().length;
-                $$.parent().append('<tr><td colspan="' + numCols + '"><a class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a></tr>');
+                $$.parent().append('<tr><td colspan="' + numCols + '"><a class="btn btn_primary  pull-right" href="javascript:void(0)"><i class="icon-plus"></i> ' + options.addText + '</a></tr>');
                 addButton = $$.parent().find('tr:last a');
                 addButton.parents('tr').addClass(options.formCssClass + '-add');
             } else {
                 // Otherwise, insert it immediately after the last form:
-                $$.filter(':last').after('<a class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a>');
+                $$.filter(':last').after('<a class="btn btn_primary  pull-right" href="javascript:void(0)"><i class="icon-plus"></i> ' + options.addText + '</a>');
                 addButton = $$.filter(':last').next();
             }
             addButton.click(function() {
@@ -157,8 +157,8 @@
     $.fn.formset.defaults = {
         prefix: 'form',                  // The form prefix for your django formset
         formTemplate: null,              // The jQuery selection cloned to generate new form instances
-        addText: 'add another',          // Text for the add link
-        deleteText: 'remove',            // Text for the delete link
+        addText: 'Add Another',          // Text for the add link
+        deleteText: 'Remove',            // Text for the delete link
         addCssClass: 'add-row',          // CSS class applied to the add link
         deleteCssClass: 'delete-row',    // CSS class applied to the delete link
         formCssClass: 'dynamic-form',    // CSS class applied to each form in a formset
