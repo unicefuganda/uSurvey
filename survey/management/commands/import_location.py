f6d577775d6008d515a2f72504a2164fa5cf1ea9
antsmc2 <antsmc2@yahoo.com>
2015-08-31 22:59:18 +0100

    improved upload script performance. fixed a previous bug

--- survey/management/commands/import_location.py
+++ survey/management/commands/import_location.py
@@ -28,6 +28,8 @@ class Command(BaseCommand):
             location_types.append(location_type)
         existing_locs = dict([((loc.name, loc.parent.pk), loc) for loc in Location.objects.all() if loc.parent])
         existing_eas = dict([(ea.name, ea) for ea in EnumerationArea.objects.all()])
+        created_eas = {}
+        created_locs = {}
         for items in csv_file:
             for index, item in enumerate(items):
                 if index == 0:
@@ -35,23 +37,31 @@ class Command(BaseCommand):
                 print 'current index: ', index
                 if has_ea and index == self.EA_INDEX:
                     print 'loading EA ', item.strip()
-                    ea = existing_eas.get(item.strip(), None)
-                    if ea is None:
-                        ea = EnumerationArea.objects.create(name=item.strip(),
-                                                    total_households=self.DEFAULT_TOTAL_HOUSEHOLDS)
-                        ea.locations.add(location)
-                        ea.save()
+                    ea = created_eas.get(item.strip(), None)
+                    if ea is None: #not already created
+                        ea = existing_eas.get(item.strip(), None)
+                        if ea is None:
+                            ea = EnumerationArea.objects.create(name=item.strip(),
+                                                        total_households=self.DEFAULT_TOTAL_HOUSEHOLDS)
+                            ea.locations.add(location)
+                            ea.save()
+                        created_eas[item.strip()] = ea
                     break
                 print 'loading item ', item
+                parent = location
                 if location is None:
-                    location, _ = Location.objects.get_or_create(name=item.strip(), type=location_types[index], parent=location)
+                    location, _ = Location.objects.get_or_create(name=item.strip(), type=location_types[index], parent=parent)
                 else:
-                    location = existing_locs.get((item.strip(), location.pk),
+                    location = created_locs.get((item.strip(), parent.pk), None)
+                    if location is None: #if not already created
+                        location = existing_locs.get((item.strip(), parent.pk), #check if exsiting
                                           Location.objects.create(name=item.strip(),
-                                                                  type=location_types[index], parent=location)
+                                                                  type=location_types[index], parent=parent)
                                           )
+                    created_locs[(item.strip(), parent.pk)] = location
                 print 'loading ', location
         #clean up... delete locations and types having no name
         LocationType.objects.filter(name='').delete()
         Location.objects.filter(name='').delete()
+        EnumerationArea.objects.filter(name='').delete()
         self.stdout.write('Successfully imported!')
