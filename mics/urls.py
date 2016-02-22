7ba7df3ff1842f27b7bcfe05f1bd0ca0cc68c48a
antsmc2 <antsmc2@yahoo.com>
2016-02-22 18:57:15 +0100

    modified mics/urls.py to handle static files if not configured on webserver

--- mics/urls.py
+++ mics/urls.py
@@ -18,9 +18,8 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 ) + survey_urls
 
-#Static content serving for development
-if settings.DEBUG:
-    urlpatterns += patterns('',
+#Static content serving if not handled by web server
+urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
         )
