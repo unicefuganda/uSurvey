917b7045b23deb4f0a47cd8bfd5adf4a56ee6294
antsmc2 <antsmc2@yahoo.com>
2016-04-15 13:54:08 +0100

    fixed interviewer problem with adding ussd field.

--- survey/context_processor.py
+++ survey/context_processor.py
@@ -2,7 +2,7 @@ from django.conf import settings
 # from survey.interviewer_configs import COUNTRY_PHONE_CODE
 
 def context_extras (request):
-    generals = { 'PROJECT_TITLE': settings.PROJECT_TITLE, 'country_phone_code' : settings.COUNTRY_CODE }
+    generals = { 'PROJECT_TITLE': settings.PROJECT_TITLE, 'country_phone_code' : settings.COUNTRY_PHONE_CODE }
     if request.GET:
         generals['q'] = request.GET.get('q', '')
     return generals
