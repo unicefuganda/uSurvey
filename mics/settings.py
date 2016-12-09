2b79af18cdd36b107c30391d04c40ef62f4184de
antsmc2 <antsmc2@yahoo.com>
2016-12-09 09:29:34 +0100

    HTML changes

--- mics/settings.py
+++ mics/settings.py
@@ -174,6 +174,7 @@ INSTALLED_APPS = (
     'django_rq',
     'django_rq_dashboard',
     'channels',
+    'macros'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
