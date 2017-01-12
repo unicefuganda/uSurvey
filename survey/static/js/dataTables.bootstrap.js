96e1369d27b96ba7afece270f6418380d5206446
sudheer <sudheer.s@dhanushinfotech.net>
2017-01-12 18:14:49 +0530

    UI Button,Pagination,Tables Headersfixed and some styling of page footer

--- survey/static/js/dataTables.bootstrap.js
+++ survey/static/js/dataTables.bootstrap.js
@@ -43,12 +43,12 @@ $.extend( $.fn.dataTableExt.oPagination, {
 				}
 			};
 
-			$(nPaging).addClass('pagination').append(
+			/*$(nPaging).addClass('pagination').append(
 				'<ul>'+
 					'<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
 					'<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
 				'</ul>'
-			);
+			);*/
 			var els = $('a', nPaging);
 			$(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
 			$(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
