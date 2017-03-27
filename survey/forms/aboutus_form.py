bc06fe077a91d1c0af8c974e5e1d7d126c7f467e
santhosh <eskguptha@gmail.com>
2017-03-27 19:48:49 +0530

    uServey Success story changes

--- survey/forms/aboutus_form.py
+++ survey/forms/aboutus_form.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, forms
-from survey.models import AboutUs
+from survey.models import AboutUs, SuccessStories
 
 
 class AboutUsForm(ModelForm):
@@ -10,3 +10,13 @@ class AboutUsForm(ModelForm):
             'content': forms.Textarea(attrs={"rows": 10, 'cols': 40, "id": "content-editor"})
         }
         exclude = []
+
+class SuccessStoriesForm(ModelForm):
+	print "hello"
+
+	class Meta:
+		model = SuccessStories
+		widgets = {
+			'content': forms.Textarea(attrs={"rows": 10, 'cols': 40, "id": "content-editor"})
+		}
+		exclude = []
