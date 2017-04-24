05a4e818c3744b82b6a70df7f99e07c283fd7aeb
santhosh <eskguptha@gmail.com>
2017-04-24 19:29:23 +0530

    url changs

--- mics/urls.py
+++ mics/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns
 from django.contrib import admin
 from survey.urls import urlpatterns as survey_urls
 from django.conf import settings
+from django.conf.urls import (
+handler400, handler403, handler404, handler500
+)
 
 admin.autodiscover()
 
@@ -22,3 +25,9 @@ urlpatterns += patterns('',
                             {'document_root': settings.MEDIA_ROOT,
                              'show_indexes': False}),
                         )
+
+handler400 = 'survey.views.home_page.custom_400'
+handler403 = 'survey.views.home_page.custom_403'
+handler404 = 'survey.views.home_page.custom_404'
+handler500 = 'survey.views.home_page.custom_500'
+
